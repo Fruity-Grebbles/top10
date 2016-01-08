a41094231211596f70c026eb9d575b25c2000818
Josh <josh@localhost.localdomain>
2016-01-07 21:07:18 -0500

    further improved pyinstaller support

--- downloader/sources/__init__.py
+++ downloader/sources/__init__.py
@@ -1,7 +1,7 @@
-from os.path import dirname, basename, isfile
+from os import path
 import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-sources = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') ]
+modules = glob.glob(path.dirname(__file__)+"/*.py")
+sources = [ path.basename(f)[:-3] for f in modules if path.isfile(f) and not f.endswith('__init__.py') ]
 __all__ = sources
 
 
