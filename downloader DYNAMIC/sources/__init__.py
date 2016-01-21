3bc6ee82b6c2ffcaac1b5e03ae673429517be43a
Josh <josh@localhost.localdomain>
2016-01-21 17:37:29 -0500

    removed unbuildable dynamic source configuration

--- downloader DYNAMIC/sources/__init__.py
+++
@@ -1,7 +0,0 @@
-from os import path
-import glob
-modules = glob.glob(path.dirname(__file__)+"/*.py")
-sources = [ path.basename(f)[:-3] for f in modules if path.isfile(f) and not f.endswith('__init__.py') ]
-__all__ = sources
-
-
