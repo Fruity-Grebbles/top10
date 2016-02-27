204af6d41aa9b7842d6ef6dc0172297ad56902aa
Josh <josh@localhost.localdomain>
2016-02-26 20:45:25 -0500

    Finished urllib implimentation

--- downloader/__init__.py
+++ downloader/__init__.py
@@ -15,15 +15,12 @@ def download(url,logfunc,outfile):
 	f = open(outfile,"wb+")
 	site = urllib.urlopen(url)
 	meta = site.info()
-	filesize = meta.getheaders("Content-Length")[0]
+	filesize = float(meta.getheaders("Content-Length")[0])
+	logfunc(filesize)
 	while True:
-		block = f.read(4096) # read a 4k block from the file
+		block = site.read(4096) # read a 4k block from the file
 		if len(block) == 0:
 			break
 		f.write(block)
-		logfunc(str(os.stat(filename).st_size/filesize*100))
+		logfunc(str(os.stat(outfile).st_size/filesize*100))
 	f.close()
-def sprint(x):
-	print x
-
-download("http://musicjustfor.me/assets/songs/39000-39999/39381-take-on-me-a-ha--1411576443.mp3?dl=1",sprint,"this.mp3")
